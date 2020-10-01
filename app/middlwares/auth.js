20146adb0384f77cf32caac3d43368386bd4b825
EwertonPSA <ewertonpsa2009@usp.br>
2020-10-01 20:04:38 -0300

    construido um formato pra agenda e incluido icones

--- app/middlwares/auth.js
+++ app/middlwares/auth.js
@@ -4,22 +4,34 @@ const authConfig = require('../../config/auth');
 function authMiddleware(req, res, next){
     const authHeader = req.headers.authorization;
     if(!authHeader){
-        return res.status(401).send({error: {msg:'No token provided'}});
+        return res.status(401).send({error: {
+            type: 'ErrorMiddlware',
+            msg:'Token não informado'
+        }});
     }
     const parts = authHeader.split(' ');
     if(!(parts.length === 2)){
-        return res.status(401).send({error: {msg:'Token error'}});
+        return res.status(401).send({error: {
+            type: 'ErrorMiddlware',
+            msg:'Erro no token'
+        }});
     }
 
     //Usando desestruturacao para acessar as partes
     const [scheme, token] = parts;
     //Verificando se no scheme ta o escrito no inicio o Bearer (ignorando o que tem a frente depois)
     if(!/^Bearer$/i.test(scheme)){
-        return res.status(401).send({error: {msg:'Token malformatted'}});
+        return res.status(401).send({error: {
+            type: 'ErrorMiddlware',
+            msg:'Token malformatted'
+        }});
     }
 
     jwt.verify(token, authConfig.secret, (err, decoded) => {
-        if(err) return res.status(401).send({error: {msg:'Token invalid'}});
+        if(err) return res.status(401).send({error: {
+            type: 'ErrorMiddlware',
+            msg:'token expirado'
+        }});
         req.userId = decoded.id;
         return next();
     });
--- public/js/principal.js
+++ public/js/principal.js
@@ -7,11 +7,19 @@ function Mudarestado(el) {
         document.getElementById(el).style.display = 'none';
 };
 
+function incluirMsgErroDeAutenticacao(identificador, msg){
+    document.querySelector(identificador).innerHTML = msg;
+}
+
 function usuarioComAutenticacao(){
     $('#usuario-info').show();
     $('.usuario-autenticado-eventos').show();
     $('#btn-register-login').hide();
     $('#btn-desconectar').show()
+    $('.trash_link').hide();
+    $('.edit_link').hide();
+    
+    document.querySelector(".Titulo_Consulta_Evento").innerHTML = 'Seus Eventos Do Dia';
     document.querySelector("#usuario-info i").innerHTML = localStorage.getItem('nome_usuario');
 };
 
@@ -20,7 +28,41 @@ function usuarioSemAutenticacao(){
     $('.usuario-autenticado-eventos').hide();
     $('#btn-register-login').show();
     $('#btn-desconectar').hide()
+    $('.trash_link').hide();
+    $('.edit_link').hide();
+    document.querySelector(".Titulo_Consulta_Evento").innerHTML = 'Todos Eventos Do Dia';
 };
+
+function limpar_dados_usuario(){
+    localStorage.removeItem('token_usuario');
+    localStorage.removeItem('nome_usuario');
+    localStorage.removeItem('email_usuario');
+}
+
+/**
+ * Repassa o token para obter o id a partir do middlware.
+ * Busca eventos que possui o id do usuario como criacao
+ * E esteja na data atual
+ * Model usuario TEM Q GUARDAR O ID DO USUARIO, NESSA ETAPA
+ * O USUARIO NAO PRECISA GUARDAR OS ID DOS EVENTOS, APENAS
+ * SE EU FIZESSE UM SISTEMA DE CONVITE(NAO VAI ROLAR)
+ * 
+ * 
+ * BUSCAR EVENTOS OU QUE COMECAM HOJE, OU QUE TEMINAM HOJE
+ * OU AMBOS-> OR
+ */
+async function consultaEventosDoUsuarioNoDia(){
+    const eventos = [];
+    return eventos;
+}
+
+/**
+ * Busca eventos que esteja na data atual
+ */
+async function consultaTodosEventosDoDia(){
+    const eventos = [];
+    return eventos;
+}
 
 async function fazerLogin(event, form){
     event.preventDefault();//Evita q chame o GET por padrao
@@ -37,11 +79,14 @@ async function fazerLogin(event, form){
             },
         };
         await fetch('http://localhost:9001/auth/authenticate', option)
-        .then(res => res.json())
+        .then(res => {
+            return res.json();
+        })
         .then(json => {
             if(json.hasOwnProperty('error')){
-                alert(json.error.msg);
+                incluirMsgErroDeAutenticacao("#msg_form_login b", json.error.msg);
             }else{
+                incluirMsgErroDeAutenticacao(".msg_error_auth b", "");
                 localStorage.setItem('token_usuario', json.token);
                 localStorage.setItem('nome_usuario', json.user.name);
                 localStorage.setItem('email_usuario', json.user.email);
@@ -78,8 +123,9 @@ async function fazerCadastro(event, form){
         })
         .then(json => {
             if(json.hasOwnProperty('error')){
-                alert(json.error.msg);
+                incluirMsgErroDeAutenticacao("#msg_form_register b", json.error.msg);
             }else{
+                incluirMsgErroDeAutenticacao(".msg_error_auth b", "");
                 localStorage.setItem('token_usuario', json.token);
                 localStorage.setItem('nome_usuario', json.user.name);
                 localStorage.setItem('email_usuario', json.user.email);
@@ -90,22 +136,30 @@ async function fazerCadastro(event, form){
     }
 };
 
-
-
 $(document).ready(function(){
     usuarioAutenticado = (localStorage.getItem('token_usuario') !== null);
+    
+    /*A partir da autenticacao escondemos ou exibimos funcionalidades*/
     if(usuarioAutenticado === true){ 
         usuarioComAutenticacao();
-        //Busca o nome do usuario autenticado para incluir no html
-        
-    }else{//usuario nao autenticado
+        consultaEventosDoUsuarioNoDia()
+        .then(eventos=>{
+            eventos.length===0 ? $('.nenhumEvento').show(): $('.nenhumEvento').hide();
+        }).catch(err=>{
+            console.log(err);
+        });
+    }else{
         usuarioSemAutenticacao();
+        consultaTodosEventosDoDia()
+        .then(eventos=>{
+            eventos.length===0 ? $('.nenhumEvento').show(): $('.nenhumEvento').hide();
+        }).catch(err=>{
+            console.log(err);
+        });
     }
 
     $('#btn-desconectar').click( function(){
-        localStorage.removeItem('token_usuario');
-        localStorage.removeItem('nome_usuario');
-        localStorage.removeItem('email_usuario');
+        limpar_dados_usuario();
     });
 
     $("#buscar").click(async function(){
