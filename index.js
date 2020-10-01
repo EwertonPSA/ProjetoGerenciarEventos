ac8cefdc1113c4c9c444ed9a6a69c1e7715a41e2
EwertonPSA <ewertonpsa2009@usp.br>
2020-10-01 11:48:38 -0300

    retirado views

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 "use strict";
-const path = require("path");
+//const path = require("path");
 const express = require('express');
 const app = express();
-const handlebars = require('express-handlebars');
+//const handlebars = require('express-handlebars');
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 dotenv.config();
 
-app.set('view engine', 'hbs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
 
 app.use(express.static(__dirname + '/public'));
 
-app.set("views", path.join(__dirname, '/app/views'));
-
-app.engine('hbs', handlebars({
-    layoutsDir: `${__dirname}/app/views/layouts`,
-    extname: 'hbs',
-    defaultLayout: 'index'
-}));
-
 require('./routes/route')(app)
-app.listen(9001, function(){
+app.listen(process.env.DEFINE_PORT_NODE, function(){
     console.log("listen");
 });
