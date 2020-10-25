734826702f4a3a04bb49c0fce661e7f165dae88c
EwertonPSA <ewertonpsa2009@usp.br>
2020-10-25 20:49:17 -0300

    movido pra pasta server

--- app/controllers/controllerPgPrincipal.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-const principal = (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public', 'principal.html'));
-};
-
-module.exports = {
-    principal
-};
--- config/auth.js
+++
@@ -1,5 +0,0 @@
-// Gera um valores fortemente criptografados toda vez que a aplicacao
-// Inicializa para criacao dos tokens
-const crypto = require('crypto');
-const secretTemporario = "7983EBCF36058727E1E3C098174A4496sakskasKAs"
-module.exports.secret = secretTemporario;//crypto.randomBytes(50).toString('hex');
